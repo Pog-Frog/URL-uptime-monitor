a455e55bb2879363dfb527bb65ddc50e5b66a434
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2024-06-16 17:28:56 +0300

    feat: models and interfaces

---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import {Document, model, Schema} from "mongoose";
+import {IUser} from "../interfaces/user.interface";
+
+
+const userSchema: Schema = new Schema({
+        name: {type: String, minlength: 3, maxlength: 50, required: [true, 'Name is required']},
+        email: {type: String, required: [true, 'Email is required'], unique: true},
+        hashedPassword: {type: String, required: [true, 'Password is required']},
+    },
+    {timestamps: true}
+);
+
+userSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.hashedPassword;
+    return obj;
+};
+
+
+export const User = model<IUser, Document>('User', userSchema);
